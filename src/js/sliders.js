1de9897ac916570e9b91f7b3486c69a96c11db96
unknown <mirtowow@gmail.com>
2022-06-02 16:54:55 -0300

    refactor(general): refactored functions to better readability and maintenance

--- src/js/sliders.js
+++ src/js/sliders.js
@@ -5,6 +5,9 @@ var sliderInterest = document.getElementById('sliderInterest');
 var outputMortgage = document.getElementById('mortgageOutput');
 var outputInterest = document.getElementById('interestOutput');
 
+const sliders = [sliderMortgage, sliderInterest];
+const outputs = [outputMortgage, outputInterest];
+
 /**
  * Allows addEventListener chaining
  */
@@ -22,40 +25,37 @@ EventTarget.prototype.addEventListener = (() => {
  * - The thumb is being moved or;
  * - The value was input manually.
  */
-/** Thumb movement */
-sliderMortgage.addEventListener('input', function () {
-  var xm = ((sliderMortgage.value - sliderMortgage.min) / (sliderMortgage.max - sliderMortgage.min)) * 100;
-  var colorMortgage = `linear-gradient(90deg, #1B3979 ${xm}%, #DDDDDD ${xm}%)`;
-  sliderMortgage.style.background = colorMortgage;
-});
 
-sliderInterest.addEventListener('input', function () {
-  var xi = ((sliderInterest.value - sliderInterest.min) / (sliderInterest.max - sliderInterest.min)) * 100;
-  var colorInterest = `linear-gradient(90deg, #1B3979 ${xi}%, #DDDDDD ${xi}%)`;
-  sliderInterest.style.background = colorInterest;
+/** Thumb movement */
+sliders.forEach((element) => {
+  element.addEventListener('input', () => {
+    var xm = ((element.value - element.min) / (element.max - element.min)) * 100;
+    var colorMortgage = `linear-gradient(90deg, #1B3979 ${xm-5}%, #DDDDDD ${xm-5}%)`;
+    element.style.background = colorMortgage;
+  });
 });
 
 /** Value input manually */
 outputMortgage
-  .addEventListener('input', function () {
+  .addEventListener('input', (event) => {
     var xm = ((sliderMortgage.value - sliderMortgage.min) / (sliderMortgage.max - sliderMortgage.min)) * 100;
-    var colorMortgage = `linear-gradient(90deg, #1B3979 ${xm}%, #DDDDDD ${xm}%)`;
+    var colorMortgage = `linear-gradient(90deg, #1B3979 ${xm-5}%, #DDDDDD ${xm-5}%)`;
     sliderMortgage.style.background = colorMortgage;
 
-    if (outputMortgage.value === '') {
+    if (outputMortgage.value === '' || (event.charCode >= 48 && event.charCode <= 57)) {
       sliderMortgage.style.background = `linear-gradient(90deg, #1B3979 2%, #DDDDDD 2%)`;
       sliderMortgage.value = 1;
     }
   })
-  .addEventListener('focusout', function () {
-    if (outputMortgage.value === '' || outputMortgage.value > sliderMortgage.max || sliderMortgage.value < outputMortgage.min) {
+  .addEventListener('focusout', () => {
+    if (outputMortgage.value === '' || outputMortgage.value > sliderMortgage.max || outputMortgage.value < sliderMortgage.min) {
       outputMortgage.value = sliderMortgage.value = 1;
       sliderMortgage.style.background = `linear-gradient(90deg, #1B3979 2%, #DDDDDD 2%)`;
     }
   });
 
 outputInterest
-  .addEventListener('input', function () {
+  .addEventListener('input', () => {
     var xi = ((sliderInterest.value - sliderInterest.min) / (sliderInterest.max - sliderInterest.min)) * 100;
     var colorInterest = `linear-gradient(90deg, #1B3979 ${xi}%, #DDDDDD ${xi}%)`;
     sliderInterest.style.background = colorInterest;
@@ -70,14 +70,21 @@ outputInterest
       sliderInterest.value = 1;
     }
   })
-  .addEventListener('focusout', function () {
-    // Handles the case where the user enters the value "10." 
+  .addEventListener('focusout', () => {
+    // Handles the case where the user enters the value "10."
     if (outputInterest.value.slice(-1) === '.') {
       outputInterest.value = outputInterest.value.slice(0, -1);
     }
 
-    if (outputInterest.value === '' || outputInterest.value > sliderInterest.max || sliderInterest.value < outputInterest.min) {
+    if (
+      outputInterest.value === '' ||
+      outputInterest.value > sliderInterest.max ||
+      outputInterest.value < sliderInterest.min ||
+      !outputInterest.value.match(/^[0-9]*\.?[0-9]*$/)
+    ) {
       outputInterest.value = sliderInterest.value = 1;
       sliderInterest.style.background = `linear-gradient(90deg, #1B3979 10%, #DDDDDD 10%)`;
     }
   });
+
+//              onkeypress="return event.charCode >= 48 && event.charCode <= 57"
--- src/js/textInputs.js
+++ src/js/textInputs.js
@@ -1,8 +1,8 @@
 var amountInput = document.getElementById('amount-input');
 var taxInput = document.getElementById('tax-input');
 var insuranceInput = document.getElementById('insurance-input');
-var mortgageSliderOutput = document.getElementById('mortgage-output');
-var interestSlider = document.getElementById('slider-interest');
+
+const textInputs = [amountInput, taxInput, insuranceInput];
 
 var formatter = new Intl.NumberFormat('en-US', {
   style: 'currency',
@@ -24,85 +24,34 @@ EventTarget.prototype.addEventListener = (() => {
  * Applies formatting on focus and focusout events,
  * as well as do not let the user delete the currency prefix.
  */
-amountInput
-  .addEventListener('focus', function () {
-    // Places the cursor after the last digit
-    const eol = amountInput.value.length;
-    amountInput.setSelectionRange(eol, eol);
 
-    if (amountInput.value === '$ 0.00') {
-      amountInput.value = '$ ';
-    } else {
-      amountInput.value = amountInput.value.replace(/[^0-9.$ ]/g, '');
-      amountInput.value = amountInput.value.replace(/\.00$/, '');
-    }
-  })
-  .addEventListener('input', function () {
-    if (amountInput.value.length < 3) {
-      amountInput.value = '$ ';
-    }
-  })
-  .addEventListener('focusout', function () {
-    amountInput.value = amountInput.value.replace(/^\$/, '');
+textInputs.forEach((element) => {
+  element
+    .addEventListener('focus', () => {
+      // Places the cursor after the last digit
+      const eol = element.value.length;
+      element.setSelectionRange(eol, eol);
 
-    amountInput.value === ' '
-      ? (amountInput.value = '$ ')
-      : (amountInput.value = formatter
-          .format(amountInput.value)
-          .replace(/^(\D+)/, '$1 ')
-          .slice(0, -3));
-  });
+      if (element.value === '$ 0.00') {
+        element.value = '$ ';
+      } else {
+        element.value = element.value.replace(/[^0-9.$ ]/g, '');
+        element.value = element.value.replace(/\.00$/, '');
+      }
+    })
+    .addEventListener('input', () => {
+      if (element.value.length < 3) {
+        element.value = '$ ';
+      }
+    })
+    .addEventListener('focusout', () => {
+      element.value = element.value.replace(/^\$/, '');
 
-taxInput
-  .addEventListener('focus', function () {
-    const eol = taxInput.value.length;
-    taxInput.setSelectionRange(eol, eol);
-
-    if (taxInput.value === '$ 0.00') {
-      taxInput.value = '$ ';
-    } else {
-      taxInput.value = taxInput.value.replace(/[^0-9.$ ]/g, '');
-      taxInput.value = taxInput.value.replace(/\.00$/, '');
-    }
-  })
-  .addEventListener('input', function () {
-    if (taxInput.value.length < 3) {
-      taxInput.value = '$ ';
-    }
-  })
-  .addEventListener('focusout', function () {
-    taxInput.value = taxInput.value.replace(/^\$/, '');
-    taxInput.value === ' '
-      ? (taxInput.value = '$ ')
-      : (taxInput.value = formatter
-          .format(taxInput.value)
-          .replace(/^(\D+)/, '$1 ')
-          .slice(0, -3));
-  });
-
-insuranceInput
-  .addEventListener('focus', function () {
-    const eol = insuranceInput.value.length;
-    insuranceInput.setSelectionRange(eol, eol);
-
-    if (insuranceInput.value === '$ 0.00') {
-      insuranceInput.value = '$ ';
-    } else {
-      insuranceInput.value = insuranceInput.value.replace(/[^0-9.$ ]/g, '');
-      insuranceInput.value = insuranceInput.value.replace(/\.00$/, '');
-    }
-  })
-  .addEventListener('input', function () {
-    if (insuranceInput.value.length < 3) {
-      insuranceInput.value = '$ ';
-    }
-  })
-  .addEventListener('focusout', function () {
-    insuranceInput.value = insuranceInput.value.replace(/^\$/, '');
-    insuranceInput.value === ' '
-      ? (insuranceInput.value = '$ ')
-      : (insuranceInput.value = formatter
-          .format(insuranceInput.value)
-          .replace(/^(\D+)/, '$1 ')
-          .slice(0, -3));
-  });
+      element.value === ' '
+        ? (element.value = '$ ')
+        : (element.value = formatter
+            .format(element.value)
+            .replace(/^(\D+)/, '$1 ')
+            .slice(0, -3));
+    });
+});
