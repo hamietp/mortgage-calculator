0c64388dc75d51b281a9640ba879c9ae719a163d
unknown <mirtowow@gmail.com>
2022-06-02 00:03:20 -0300

    feature(layout): design, inputs and field values formatting done

--- src/js/sliders.js
+++ src/js/sliders.js
@@ -1,31 +1,83 @@
 /** Instancing slider elements */
-var sliderMortgage = document.getElementById("slider-mortgage");
-var sliderInterest = document.getElementById("slider-interest");
+var sliderMortgage = document.getElementById('sliderMortgage');
+var sliderInterest = document.getElementById('sliderInterest');
 
-var outputMortgage = document.getElementById("mortgage-output");
-var outputInterest = document.getElementById("interest-output");
-
-/** Function to update text values from the input based on where the position thumb currently is.
- * @param this.value
- */
-sliderMortgage.oninput = function () {
-  outputMortgage.value = this.value;
-};
-sliderInterest.oninput = function () {
-  outputInterest.value = this.value;
-};
+var outputMortgage = document.getElementById('mortgageOutput');
+var outputInterest = document.getElementById('interestOutput');
 
 /**
- * Modifies the background color of each slider, based on the thumb position.
+ * Allows addEventListener chaining
  */
-sliderMortgage.addEventListener("input", function () {
-  var xm = ((sliderMortgage.value - sliderMortgage.min) / (sliderMortgage.max - sliderMortgage.min) * 100);
+EventTarget.prototype.addEventListener = (() => {
+  const addEventListener = EventTarget.prototype.addEventListener;
+  return function () {
+    addEventListener.apply(this, arguments);
+    return this;
+  };
+})();
+
+/**
+ * Updates color of each slider, based on the thumb position:
+ * Color will be updated if:
+ * - The thumb is being moved or;
+ * - The value was input manually.
+ */
+/** Thumb movement */
+sliderMortgage.addEventListener('input', function () {
+  var xm = ((sliderMortgage.value - sliderMortgage.min) / (sliderMortgage.max - sliderMortgage.min)) * 100;
   var colorMortgage = `linear-gradient(90deg, #1B3979 ${xm}%, #DDDDDD ${xm}%)`;
   sliderMortgage.style.background = colorMortgage;
 });
 
-sliderInterest.addEventListener("input", function () {
-  var xi = ((sliderInterest.value - sliderInterest.min) / (sliderInterest.max - sliderInterest.min) * 100);
+sliderInterest.addEventListener('input', function () {
+  var xi = ((sliderInterest.value - sliderInterest.min) / (sliderInterest.max - sliderInterest.min)) * 100;
   var colorInterest = `linear-gradient(90deg, #1B3979 ${xi}%, #DDDDDD ${xi}%)`;
   sliderInterest.style.background = colorInterest;
 });
+
+/** Value input manually */
+outputMortgage
+  .addEventListener('input', function () {
+    var xm = ((sliderMortgage.value - sliderMortgage.min) / (sliderMortgage.max - sliderMortgage.min)) * 100;
+    var colorMortgage = `linear-gradient(90deg, #1B3979 ${xm}%, #DDDDDD ${xm}%)`;
+    sliderMortgage.style.background = colorMortgage;
+
+    if (outputMortgage.value === '') {
+      sliderMortgage.style.background = `linear-gradient(90deg, #1B3979 2%, #DDDDDD 2%)`;
+      sliderMortgage.value = 1;
+    }
+  })
+  .addEventListener('focusout', function () {
+    if (outputMortgage.value === '' || outputMortgage.value > sliderMortgage.max || sliderMortgage.value < outputMortgage.min) {
+      outputMortgage.value = sliderMortgage.value = 1;
+      sliderMortgage.style.background = `linear-gradient(90deg, #1B3979 2%, #DDDDDD 2%)`;
+    }
+  });
+
+outputInterest
+  .addEventListener('input', function () {
+    var xi = ((sliderInterest.value - sliderInterest.min) / (sliderInterest.max - sliderInterest.min)) * 100;
+    var colorInterest = `linear-gradient(90deg, #1B3979 ${xi}%, #DDDDDD ${xi}%)`;
+    sliderInterest.style.background = colorInterest;
+
+    // Handles the case where the user enters 2 decimal points
+    if (outputInterest.value.slice(-2) === '..') {
+      outputInterest.value = outputInterest.value.slice(0, -1);
+    }
+
+    if (outputInterest.value === '') {
+      sliderInterest.style.background = `linear-gradient(90deg, #1B3979 10%, #DDDDDD 10%)`;
+      sliderInterest.value = 1;
+    }
+  })
+  .addEventListener('focusout', function () {
+    // Handles the case where the user enters the value "10." 
+    if (outputInterest.value.slice(-1) === '.') {
+      outputInterest.value = outputInterest.value.slice(0, -1);
+    }
+
+    if (outputInterest.value === '' || outputInterest.value > sliderInterest.max || sliderInterest.value < outputInterest.min) {
+      outputInterest.value = sliderInterest.value = 1;
+      sliderInterest.style.background = `linear-gradient(90deg, #1B3979 10%, #DDDDDD 10%)`;
+    }
+  });
--- src/js/textInputs.js
+++ src/js/textInputs.js
@@ -1,63 +1,81 @@
 var amountInput = document.getElementById('amount-input');
 var taxInput = document.getElementById('tax-input');
 var insuranceInput = document.getElementById('insurance-input');
-var mortgageSlider = document.getElementById('slider-mortgage');
-var iinterestSlider = document.getElementById('slider-interest');
+var mortgageSliderOutput = document.getElementById('mortgage-output');
+var interestSlider = document.getElementById('slider-interest');
 
 var formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
+  style: 'currency',
+  currency: 'USD',
 });
-/** Format values before the first display */
-amountInput.value = formatter.format(amountInput.value);
-taxInput.value = formatter.format(taxInput.value);
-insuranceInput.value = formatter.format(insuranceInput.value);
 
 /**
  * Allows eventListener chaining
  */
 EventTarget.prototype.addEventListener = (() => {
-    const addEventListener = EventTarget.prototype.addEventListener;
-    return function () {
-        addEventListener.apply(this, arguments);
-        return this;
-    };
+  const addEventListener = EventTarget.prototype.addEventListener;
+  return function () {
+    addEventListener.apply(this, arguments);
+    return this;
+  };
 })();
 
 /**
  * Applies formatting on focus and focusout events
  */
-amountInput.addEventListener('focus', function () {
-    if (amountInput.value === '$0.00') {
-        amountInput.value = '';
+amountInput
+  .addEventListener('focus', function () {
+    if (amountInput.value === '$ 0.00') {
+      amountInput.value = '$ ';
     } else {
-        amountInput.value = amountInput.value.replace(/[^0-9.]/g, '');
-        amountInput.value = amountInput.value.replace(/\.00$/, '');
+      amountInput.value = amountInput.value.replace(/[^0-9.]/g, '');
+      amountInput.value = amountInput.value.replace(/\.00$/, '');
     }
-}).addEventListener('focusout', function () {
-    amountInput.value = formatter.format(amountInput.value);
-});
+  })
+  .addEventListener('focusout', function () {
+    if (amountInput.value === '$') {
+      amountInput.value = '$  ';
+    }
+    amountInput.value = amountInput.value.replace(/^\$/, '');
 
-taxInput.addEventListener('focus', function () {
-    if (taxInput.value === '$0.00') {
-        taxInput.value = '';
+    amountInput.value === ''
+      ? (amountInput.value = '$ ')
+      : (amountInput.value = formatter.format(amountInput.value).replace(/^(\D+)/, '$1 ').slice(0, -3));
+
+  });
+
+taxInput
+  .addEventListener('focus', function () {
+    if (taxInput.value === '$ 0.00') {
+      taxInput.value = '$ ';
     } else {
-        taxInput.value = taxInput.value.replace(/[^0-9.]/g, '');
-        taxInput.value = taxInput.value.replace(/\.00$/, '');
+      taxInput.value = taxInput.value.replace(/[^0-9.]/g, '');
+      taxInput.value = taxInput.value.replace(/\.00$/, '');
     }
-}).addEventListener('focusout', function () {
-    taxInput.value = formatter.format(taxInput.value);
-});
+  })
+  .addEventListener('focusout', function () {
+    taxInput.value = taxInput.value.replace(/^\$/, '');
+    taxInput.value === ''
+      ? (taxInput.value = '$ ')
+      : (taxInput.value = formatter.format(taxInput.value).replace(/^(\D+)/, '$1 ').slice(0, -3));
+  });
 
-insuranceInput.addEventListener('focus', function () {
-    if (insuranceInput.value === '$0.00') {
-        insuranceInput.value = '';
+insuranceInput
+  .addEventListener('focus', function () {
+    if (insuranceInput.value === '$ 0.00') {
+      insuranceInput.value = '$ ';
     } else {
-        insuranceInput.value = insuranceInput.value.replace(/[^0-9.]/g, '');
-        insuranceInput.value = insuranceInput.value.replace(/\.00$/, '');
+      insuranceInput.value = insuranceInput.value.replace(/[^0-9.]/g, '');
+      insuranceInput.value = insuranceInput.value.replace(/\.00$/, '');
     }
-}).addEventListener('focusout', function () {
-    insuranceInput.value = formatter.format(insuranceInput.value);
-});
+  })
+  .addEventListener('focusout', function () {
+    insuranceInput.value = insuranceInput.value.replace(/^\$/, '');
+    insuranceInput.value === ''
+      ? (insuranceInput.value = '$ ')
+      : (insuranceInput.value = formatter.format(insuranceInput.value).replace(/^(\D+)/, '$1 ').slice(0, -3));
+  });
 
-// throw error if value is not a number
+/**
+ * Todo: finish CSS focus for the input fields (sliders and loan fields)
+ */
