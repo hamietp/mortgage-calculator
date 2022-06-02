ff03801530b9ec36ef21e6f8bb731c40887044e9
unknown <mirtowow@gmail.com>
2022-06-02 00:52:02 -0300

    chore(text-inputs): updated maxlength; cursor placement when focus; css hover effects

--- src/js/textInputs.js
+++ src/js/textInputs.js
@@ -21,61 +21,88 @@ EventTarget.prototype.addEventListener = (() => {
 })();
 
 /**
- * Applies formatting on focus and focusout events
+ * Applies formatting on focus and focusout events,
+ * as well as do not let the user delete the currency prefix.
  */
 amountInput
   .addEventListener('focus', function () {
+    // Places the cursor after the last digit
+    const eol = amountInput.value.length;
+    amountInput.setSelectionRange(eol, eol);
+
     if (amountInput.value === '$ 0.00') {
       amountInput.value = '$ ';
     } else {
-      amountInput.value = amountInput.value.replace(/[^0-9.]/g, '');
+      amountInput.value = amountInput.value.replace(/[^0-9.$ ]/g, '');
       amountInput.value = amountInput.value.replace(/\.00$/, '');
     }
   })
-  .addEventListener('focusout', function () {
-    if (amountInput.value === '$') {
-      amountInput.value = '$  ';
+  .addEventListener('input', function () {
+    if (amountInput.value.length < 3) {
+      amountInput.value = '$ ';
     }
+  })
+  .addEventListener('focusout', function () {
     amountInput.value = amountInput.value.replace(/^\$/, '');
 
-    amountInput.value === ''
+    amountInput.value === ' '
       ? (amountInput.value = '$ ')
-      : (amountInput.value = formatter.format(amountInput.value).replace(/^(\D+)/, '$1 ').slice(0, -3));
-
+      : (amountInput.value = formatter
+          .format(amountInput.value)
+          .replace(/^(\D+)/, '$1 ')
+          .slice(0, -3));
   });
 
 taxInput
   .addEventListener('focus', function () {
+    const eol = taxInput.value.length;
+    taxInput.setSelectionRange(eol, eol);
+
     if (taxInput.value === '$ 0.00') {
       taxInput.value = '$ ';
     } else {
-      taxInput.value = taxInput.value.replace(/[^0-9.]/g, '');
+      taxInput.value = taxInput.value.replace(/[^0-9.$ ]/g, '');
       taxInput.value = taxInput.value.replace(/\.00$/, '');
+    }
+  })
+  .addEventListener('input', function () {
+    if (taxInput.value.length < 3) {
+      taxInput.value = '$ ';
     }
   })
   .addEventListener('focusout', function () {
     taxInput.value = taxInput.value.replace(/^\$/, '');
-    taxInput.value === ''
+    taxInput.value === ' '
       ? (taxInput.value = '$ ')
-      : (taxInput.value = formatter.format(taxInput.value).replace(/^(\D+)/, '$1 ').slice(0, -3));
+      : (taxInput.value = formatter
+          .format(taxInput.value)
+          .replace(/^(\D+)/, '$1 ')
+          .slice(0, -3));
   });
 
 insuranceInput
   .addEventListener('focus', function () {
+    const eol = insuranceInput.value.length;
+    insuranceInput.setSelectionRange(eol, eol);
+
     if (insuranceInput.value === '$ 0.00') {
       insuranceInput.value = '$ ';
     } else {
-      insuranceInput.value = insuranceInput.value.replace(/[^0-9.]/g, '');
+      insuranceInput.value = insuranceInput.value.replace(/[^0-9.$ ]/g, '');
       insuranceInput.value = insuranceInput.value.replace(/\.00$/, '');
+    }
+  })
+  .addEventListener('input', function () {
+    if (insuranceInput.value.length < 3) {
+      insuranceInput.value = '$ ';
     }
   })
   .addEventListener('focusout', function () {
     insuranceInput.value = insuranceInput.value.replace(/^\$/, '');
-    insuranceInput.value === ''
+    insuranceInput.value === ' '
       ? (insuranceInput.value = '$ ')
-      : (insuranceInput.value = formatter.format(insuranceInput.value).replace(/^(\D+)/, '$1 ').slice(0, -3));
+      : (insuranceInput.value = formatter
+          .format(insuranceInput.value)
+          .replace(/^(\D+)/, '$1 ')
+          .slice(0, -3));
   });
-
-/**
- * Todo: finish CSS focus for the input fields (sliders and loan fields)
- */
