f41a939274367658485cb7f4393b3ec0cac232a6
unknown <mirtowow@gmail.com>
2022-06-03 20:28:44 -0300

    feature(calculation): finished calculation method with error handling

--- src/js/calculate.js
+++ src/js/calculate.js
@@ -9,41 +9,62 @@ EventTarget.prototype.addEventListener = (() => {
   };
 })();
 
-var submitButton = document.getElementById('btn-submit');
-var amountInput = document.getElementById('amount-input');
-var taxInput = document.getElementById('tax-input');
-var insuranceInput = document.getElementById('insurance-input');
+let homePrice = document.getElementById('homePrice'); // Home Price
+let downPayment = document.getElementById('downPayment'); // Down Payment
+let yearsOfMortgage = document.getElementById('yearsOfMortgage'); // Loan Term
+let interestRate = document.getElementById('interestRate'); // Interest Rate (%)
+let loanAmountPercentage = document.getElementById('loanAmountPercentage'); // Loan Amount (%)
+let totalMonthlyPayment = document.getElementById('totalMonthlyPayment'); // Total Monthly Payment
 
-const textInputs = [amountInput, taxInput, insuranceInput];
+let submitButton = document.getElementById('btn-submit');
 
 /**
- * Adds the data-error attribute for each input that were not filled out correctly
- *       and prevents the form from being submitted.
+ * I declared the inputs twice, because:
+ * 1) To loop through the inputs in order to check if they are valid and
+ *    also to setAttribute for data-error in each field.
  *
- * Note: only works if the input was *unchanged* at all.
- *       If the user inputs any value (like '$0'), the calculation will begin.
+ * 2) To get the values and convert them to numbers.
  *
- * @event submit
- * @param {event} event
+ *    I did this because I was getting an scope value error when changing the value
+ * a second time after submitting the form.
+ *    When that happened, the values were not getting updated and the calculation
+ * was not right or an error was popping.
+ *
  */
-submitButton.addEventListener('click', (event) => {
-  if (amountInput.value === '$ ' || taxInput.value === '$ ' || insuranceInput.value === '$ ') {
-    event.preventDefault();
-  }
 
-  textInputs.forEach((input) => {
-    if (input.value === '$ ') {
-      input.parentElement.setAttribute('data-error', 'Mandatory field');
+submitButton.addEventListener('click', (event) => {
+  let homePriceValue = event.currentTarget.form.homePrice.value; // Home Price
+  let downPaymentValue = event.currentTarget.form.downPayment.value; // Down Payment
+  let yearsOfMortgageValue = Number(event.currentTarget.form.yearsOfMortgage.value); // Loan Term
+  let interestRateValue = Number(event.currentTarget.form.interestRate.value); // Interest Rate (%)
+
+  let calcHomePrice = Number(homePriceValue.replace(/[^0-9]/g, ''));
+  let calcDownPayment = Number(downPaymentValue.replace(/[^0-9]/g, ''));
+  const userInputs = [homePrice, downPayment, interestRate];
+
+  let loanAmount = calcHomePrice - calcDownPayment;
+
+  /** Error message for each field that actually has errors */
+  userInputs.forEach((input) => {
+    if (input.value === '$' || input.value === '') {
+      event.preventDefault();
+      input.parentElement.setAttribute('data-error', 'Mandatory field') |
+        input.parentElement.parentElement.setAttribute('data-error', 'Mandatory field');
     }
   });
-});
 
-textInputs.forEach((element) => {
-  /**
-   * Removes the data-error attribute from each input separately
-   * when the user focus the input again.
-   */
-  element.addEventListener('focus', () => {
-    element.parentElement.removeAttribute('data-error');
-  });
+  if (calcDownPayment > calcHomePrice) {
+    downPayment.parentElement.parentElement.setAttribute('data-error', 'Down payment cannot be greater than home price');
+    event.preventDefault();
+  } else if (interestRateValue < 0) {
+    interestRate.parentElement.setAttribute('data-error', 'Interest cannot be less than 0');
+    event.preventDefault();
+  }
+  let totalMonthlyPayment = 0;
+  let formula =
+    ((interestRateValue / 100 / 12) * loanAmount) / (1 - Math.pow(1 + interestRateValue / 100 / 12, -yearsOfMortgageValue * 12));
+
+  isNaN(formula) ? (totalMonthlyPayment = '$ --') : (totalMonthlyPayment = formula.toFixed(2));
+
+  totalMonthlyPayment.innerText = `$ ${totalMonthlyPayment}`;
 });
